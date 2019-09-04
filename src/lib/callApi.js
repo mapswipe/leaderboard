3d3df72dca848d2554e7c1ab39b86391c62735e2
Mamadou Ouologuem <mamadou.ouologuem02@gmail.com>
2019-09-03 17:39:19 -0700

    📦 Correction of PR #3

--- src/lib/callApi.js
+++ src/lib/callApi.js
@@ -6,48 +6,33 @@ const config = require('./config');
 let localData = [];
 try {
   // eslint-disable-next-line global-require
-  localData = require('./msf-mapswipe-users-export.json');
+  localData = require('./msf-mapswipe-users-export.json').data;
   // eslint-disable-next-line no-console
 } catch (e) { console.error(e); }
 
-// If eq 'dev' fetch data locally else (eq 'prod') fetch data from Firebase
-const SOURCE = 'prod';
-
 firebase.initializeApp(config);
 const db = firebase.database();
+const companies = ['apple', 'uber', 'slack', 'github', 'lyft', 'twitter', 'amazon', 'google', 'ibm', 'sap'];
 
 const isInclude = (str, pattern) => str.toLowerCase().includes(pattern.toLowerCase());
 
-export const getLocalData = (query = undefined) => {
-  const companies = [
-    'apple',
-    'uber',
-    'slack',
-    'github',
-    'lyft',
-    'twitter',
-    'amazon',
-    'google',
-    'ibm',
-    'sap',
-  ];
+const getFormattedData = (snapshot, query = undefined) => {
   const data = [];
   let totalContributions = 0;
   let totalDistance = 0;
-  Object.keys(localData).forEach((key, index) => {
-    const { contributions, distance } = localData[key];
-    let { username } = localData[key];
-    username = username.trim();
-    const company = companies[Math.floor(index % companies.length)];
-    const formattedUsername = `${company}_${username}`;
+
+  snapshot.forEach((datum, index) => {
+    let username = '';
+    if (process.env.REACT_APP_SOURCE === 'dev') {
+      username = `${companies[Math.floor(index % companies.length)]}_${datum.username}`;
+    } else {
+      datum = datum.val();
+      username = datum.username.trim();
+    }
+    const { contributions, distance } = datum;
     const level = getLevelForContributionCount(distance);
-    if (!query || isInclude(formattedUsername, query)) {
-      data.push({
-        contributions,
-        distance,
-        username: formattedUsername,
-        level,
-      });
+    if (!query || isInclude(username, query)) {
+      data.push({ contributions, distance, username, level });
       totalContributions += contributions;
       totalDistance += distance;
     }
@@ -55,43 +40,26 @@ export const getLocalData = (query = undefined) => {
   return { data, totalContributions, totalDistance };
 };
 
-/**
- * @return Promise of all Users data where User username match with query
- * @param query string
- */
-export const getUsersPromise = (query = '') => {
-  // local data
-  if (SOURCE === 'dev') {
-    return new Promise((resolve, reject) => {
-      const res = getLocalData(query);
-      if (res) resolve(res); else reject(Error('Something goes wrong'));
-    });
-  }
+const getDevData = (query = '') => (
+  new Promise((resolve, reject) => {
+    const res = getFormattedData(localData, query);
+    if (res) resolve(res); else reject(Error('Something goes wrong'));
+  })
+);
 
-  // prod data
-  let totalContributions = 0;
-  let totalDistance = 0;
+const getProdData = (query = '') => {
   const usersRef = db.ref('users');
   return usersRef.once('value')
-    .then((snapshot) => {
-      const data = [];
-      snapshot.forEach((childSnapshot) => {
-        const docData = childSnapshot.val();
-        const { contributions, distance } = docData;
-        let { username } = docData;
-        username = username.trim();
-        const level = getLevelForContributionCount(distance);
-        if (!query || isInclude(username, query)) {
-          data.push({ ...docData, username, level });
-          totalContributions += contributions;
-          totalDistance += distance;
-        }
-      });
-      return { data, totalContributions, totalDistance };
-    })
+    .then(snapshot => getFormattedData(snapshot, query))
     .catch((err) => {
       // eslint-disable-next-line no-console
       console.log('Error getting documents', err);
       return null;
     });
 };
+
+/**
+ * @return Promise of all Users data where User username match with query
+ * @param query string
+ */
+export const getUsersPromise = (process.env.REACT_APP_SOURCE === 'dev') ? getDevData : getProdData;
